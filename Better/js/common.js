85ecaa332a7937b9ba711a17104ec3942825e915
yyj <yyjlincoln@gmail.com>
2018-11-17 19:41:56 +1100

    Update website

--- Better/js/common.js
+++ Better/js/common.js
@@ -1,4 +1,3 @@
-window.onresize=resize;
 function resize(){
 //    document.getElementsByClassName("container").style.height=window.innerHeight;
 //    document.getElementsByClassName("container").style.width=window.innerWidth;
@@ -7,4 +6,23 @@ function resize(){
     $('.container').css('height',window.innerHeight);
     $('.container').css('width',window.innerWidth);
 }
-resize();
+
+function onthescrool(){
+    var a,b,c;
+    a = $(window).height();
+    var group = $("#container-intro");
+    b = $(this).scrollTop();
+    c = group.offset().top;
+    d = a*0.1;
+    if(b+c>a+d){
+       $('#enjoytitle').fadeOut();
+    } else {
+       $('#enjoytitle').fadeIn();
+    }
+}
+
+$(document).ready(function () {
+    $(window).scroll(onthescrool);
+    $(window).resize(resize);
+    resize()   
+});
